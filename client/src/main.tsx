baf98e9c339949c12d95607d212984b396406eb1
Fillbert Halim <fhalim3-c@my.cityu.edu.hk>
2025-08-07 20:23:18 +0800

    frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello, World!</h1>
-    <p>This is a simple React application.</p>
   </StrictMode>
 )
